903aa399d51efd68f415bf7d69a8b58e59fe496b
vesjocic <vesjocic@gmail.com>
2015-08-23 23:49:38 -0400

    Adding jQuery effects

--- js/main.js
+++ js/main.js
@@ -1,17 +1,8 @@
 // declaring global variable
 var myPort = {};
 
-// if there is a value you will use over and over you might want to store it as a property in your global variable
-// myPort.url = 'http://www.urlhere.com';
-
 // method to draw SVG
-// myPort.drawSVG = function() {
-
-// };
-
-// define init function
-// this starts everything going on the page
-myPort.init = function() {
+myPort.drawSVG = function() {
 	var svg = new Walkway({ 
 	    	selector: '#svgIntro',
 	    	duration: '2000', 
@@ -19,8 +10,87 @@ myPort.init = function() {
 	svg.draw();
 };
 
+// method for smooth scrolling of xlinks
+myPort.smoothScrollX = function() {
+	// on click of xlink:hrefs do the following
+	$('.linkX').on('click',function (e) {
+		// prevent default action
+		e.preventDefault();
+		// defining target variable for xlinks
+		var targetX = $($(this).attr('xlink:href'));
+		// smooth scroll to target
+		$('html, body').stop().animate({
+			scrollTop: targetX.offset().top
+		}, 600, 'swing', function () {
+			window.location.hash = targetX;
+		});
+	});
+};
+
+// method for smooth scrolling regular links
+myPort.smoothScroll = function() {
+	// on click of regular hrefs do the following
+	$('.link').on('click',function (e) {
+		// prevent default action
+		e.preventDefault();
+		// defining target variable for links
+		var target = $(this.hash);
+		// smooth scroll to target
+		$('html, body').stop().animate({
+			scrollTop: target.offset().top
+		}, 600, 'swing', function () {
+			window.location.hash = target;
+		});
+	});
+};
+
+// method to make fixed nav appear on scroll
+myPort.showNav = function() {
+	// on scroll do the following
+	$(window).scroll(function() {
+		// defining bottom of svg
+		// bottom of svg = position of top of svg + height of svg
+		var bottom_of_svg = $('#svgIntro').offset().top + $('#svgIntro').outerHeight();
+		// if the svg is completely hidden, fade in nav
+		if ( $(window).scrollTop() > bottom_of_svg ) {
+		    $('#nav').fadeIn(300);
+		} else {
+		    $('#nav').fadeOut(300);
+		}
+	});
+}
+
+// method to make divs appear on scroll
+myPort.showDivs = function() {
+	// on scroll do the following
+	$(window).scroll(function() {
+		// check location of each element
+		$('.hide').each( function(i) {
+			// defining bottom of object
+			// bottom of object = position of top of div + div height
+			var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+			// defining bottom of window
+			// bottom of window = pixels hidden above + height of current window
+			var bottom_of_window = $(window).scrollTop() + $(window).height();
+			// if the object is completely visible in the window, fade it in
+			if ( bottom_of_window > bottom_of_object ) {
+				$(this).animate({'opacity':'1'},500);
+			}
+		});
+	});
+}
+
+// define init function
+myPort.init = function() {
+	myPort.drawSVG();
+	myPort.smoothScrollX();
+	myPort.smoothScroll();
+	myPort.showNav();
+	myPort.showDivs();
+};
+
 // doc ready function calls init function
-$(document).ready(function(){
+$(document).ready(function() {
 	console.log('jQuery is working!')
   	myPort.init();
 });
